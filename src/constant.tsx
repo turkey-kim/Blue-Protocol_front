cd8e3f4b3f33811939f6dad023a5d90b252d9088
turkey-kim <rlaxmrgml@naver.com>
2023-09-26 14:28:24 +0900

    add: server 배포 URL 추가

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,2 +1,4 @@
 export const publicPath = process.env.PUBLIC_URL;
-export const serverUrl = "http://localhost:8080";
+// export const serverUrl = "http://localhost:8080";
+export const serverUrl =
+  "https://port-0-blue-protocol-server-4fju66f2clmzty3z1.sel5.cloudtype.app";
