682acf7a7e0b7f2f0d028a318b0f7399cf4618d8
yangjaehyuk <gndksakxk10@naver.com>
2023-09-27 18:44:10 +0900

    feat: 푸터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,56 @@
-import "./App.css";
-import "./font.css";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import { Outlet } from "react-router";
-import { Routes, Route } from "react-router";
-import { MyGlobalStyle, myTheme } from "./style";
-import { ThemeProvider } from "styled-components";
-import News from "./pages/News";
-import Game from "./pages/Game";
-import Guide from "./pages/Guide";
-import Database from "./pages/Database";
-import { useEffect } from "react";
-import Login from "./pages/Login";
-import { checkToken } from "./api/auth";
-import { useRecoilState } from "recoil";
-import { loginState } from "./states/atoms";
+import './App.css';
+import './font.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router';
+import { Routes, Route } from 'react-router';
+import { MyGlobalStyle, myTheme } from './style';
+import { ThemeProvider } from 'styled-components';
+import News from './pages/News';
+import Game from './pages/Game';
+import Guide from './pages/Guide';
+import Database from './pages/Database';
+import { useEffect } from 'react';
+import Login from './pages/Login';
+import { checkToken } from './api/auth';
+import { useRecoilState } from 'recoil';
+import { loginState } from './states/atoms';
 
 const arr = [
   {
-    img: "/images/news01.png",
-    category: "캐릭터",
-    title: "엄청난 소식!",
-    content: "에린제의 호위기사 티라스가 어쩌고 저쩌고",
-    time: "12시간 전",
+    img: '/images/news01.png',
+    category: '캐릭터',
+    title: '엄청난 소식!',
+    content: '에린제의 호위기사 티라스가 어쩌고 저쩌고',
+    time: '12시간 전',
   },
 
   {
-    img: "/images/news02.png",
-    category: "캐릭터",
-    title: "멋진 소식",
-    content: "의문의 방랑자 멜로프, 멜로프 멜로프 멜로프!",
-    time: "하루 전",
+    img: '/images/news02.png',
+    category: '캐릭터',
+    title: '멋진 소식',
+    content: '의문의 방랑자 멜로프, 멜로프 멜로프 멜로프!',
+    time: '하루 전',
   },
   {
-    img: "/images/news03.png",
-    category: "캐릭터",
-    title: "엄청난 소식!",
-    content: "미래에서 온 예린제! 예린제!",
-    time: "12시간 전",
+    img: '/images/news03.png',
+    category: '캐릭터',
+    title: '엄청난 소식!',
+    content: '미래에서 온 예린제! 예린제!',
+    time: '12시간 전',
   },
 ];
 
 function Dashboard() {
   useEffect(() => {
-    localStorage.setItem("news", JSON.stringify(arr));
+    localStorage.setItem('news', JSON.stringify(arr));
   }, []);
   return (
     <div>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 }
@@ -61,7 +63,7 @@ function App() {
       let result = await checkToken();
       if (result == true) {
         setAdmin(true);
-        console.log("관리자 로그인 상태입니다");
+        console.log('관리자 로그인 상태입니다');
       } else {
         setAdmin(false);
       }
