efe86c2c78cc7a6cb437462f54e7fd0e3f7aee25
turkey-kim <rlaxmrgml@naver.com>
2023-10-01 02:10:34 +0900

    add: PostNews 페이지 추가; 기능 구현X

---
+++ src/pages/PostNews.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function PostNews() {
+  return <Container>뉴스포스트페이지!</Container>;
+}
+
+export default PostNews;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: red;
+`;
