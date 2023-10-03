e17fdac507e1a8ee0b91eef4d65c831fc13ab024
turkey-kim <rlaxmrgml@naver.com>
2023-10-04 01:51:32 +0900

    feat: 뉴스업로드 api 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
 import {SERVER_URL} from '../constant';
+import {Outlet} from 'react-router';
 
 interface LoginProps {
   id: string;
   pw: string;
+}
+
+interface NewsProps {
+  title: string | undefined;
+  outline: string | undefined;
+  category: string | undefined;
+  thumbnail: string | undefined;
+  content: string | undefined;
 }
 export const submitLogin = async ({id, pw}: LoginProps) => {
   return axios.post(`${SERVER_URL}/admin/login`, {
@@ -13,7 +22,15 @@ export const submitLogin = async ({id, pw}: LoginProps) => {
 };
 
 export const uploadImage = async (formData: FormData) => {
-  return axios.post(`${SERVER_URL}/api/uploadImage`, formData).catch(err => {
-    console.error(err);
+  return axios.post(`${SERVER_URL}/api/uploadImage`, formData);
+};
+
+export const uploadNews = async ({title, outline, category, thumbnail, content}: NewsProps) => {
+  return axios.post(`${SERVER_URL}/api/uploadNews`, {
+    title: title,
+    outline: outline,
+    category: category,
+    thumbnail: thumbnail,
+    content: content,
   });
 };
