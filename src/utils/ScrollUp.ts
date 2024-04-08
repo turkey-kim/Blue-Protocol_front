22285c95e32d4c371afaf2d6f65fa59612d8628c
TeukHee <rlaxmrgml@naver.com>
2024-04-08 16:52:12 +0900

    feat: scroll-behavior 추가

--- src/utils/ScrollUp.ts
+++ src/utils/ScrollUp.ts
@@ -7,7 +7,7 @@ export default function ScrollToTop() {
 
   useEffect(() => {
     if (targetUrl.includes(pathname)) {
-      window.scrollTo(0, 0);
+      window.scrollTo({top: 0, behavior: 'smooth'});
     }
   }, [pathname]);
 
